10ec81eec7a55aef9a7a2424f59bb22b868b7494
doohinkus <doohinkus@gmail.com>
2017-10-09 11:28:18 -0700

    add optimal delay for pcc scrub

--- portland-picture-books.py
+++ portland-picture-books.py
@@ -33,17 +33,19 @@ for key in books:
     pcc_url = "http://alliance-primo.hosted.exlibrisgroup.com/primo_library/libweb/action/search.do?fn=search&ct=search&initialSearch=true&mode=Advanced&tab=default_tab&indx=1&dum=true&srt=rank&vid=PCC&frbg=&tb=t&vl%28401499236UI0%29=isbn&vl%28401499236UI0%29=title&vl%28401499236UI0%29=isbn&vl%281UIStartWith0%29=contains&vl%28freeText0%29=" +key + "&vl%28boolOperator0%29=AND&vl%28401499248UI1%29=any&vl%28401499248UI1%29=title&vl%28401499248UI1%29=any&vl%281UIStartWith1%29=contains&vl%28freeText1%29=&vl%28boolOperator1%29=AND&vl%28401499252UI2%29=any&vl%28401499252UI2%29=title&vl%28401499252UI2%29=any&vl%281UIStartWith2%29=contains&vl%28freeText2%29=&vl%28boolOperator2%29=AND&vl%28D309595628UI3%29=all_items&vl%28743647703UI4%29=all_items&vl%28700666352UI5%29=all_items&vl%28309595632UI6%29=00&vl%28309595633UI6%29=00&vl%28309595634UI6%29=Year&vl%28309595635UI6%29=00&vl%28309595636UI6%29=00&vl%28309595637UI6%29=Year&scp.scps=scope%3A%28PCC%29&Submit=Search"
     pcc_page = urllib2.urlopen(pcc_url)
     # sleep to avoid connection reset--max is 20 connections
-    time.sleep(3)
+    count += 1
+    print (count%3)
+    if (count % 3 == 0):
+        time.sleep(9)
     # parse page
     pcc_soup = BeautifulSoup(pcc_page, 'lxml')
     # find links/titles, the links contain the isbn!!!!
     pcc_result = pcc_soup.find('div', {'id' : 'resultsNumbersTile'})
     if (int(pcc_result.h1.em.text.strip()) == 0):
         print (pcc_result.h1.em.text.strip(), key, books.get(key))
-    # print (key, books.get(key))
-        # # write info to csv
-        # with open('books.csv', 'a') as f:
-        #     writer = csv.writer(f)
-        #     # isbn is the third element in the split array
-        #     writer.writerow([link.text.strip(), isbn[2]])
-        #     f.close()
+        # write info to csv
+        with open('buy_books.csv', 'a') as f:
+            writer = csv.writer(f)
+            # isbn is the third element in the split array
+            writer.writerow([key, books.get(key)])
+            f.close()
